0f805ee67bdc84045828f626a9aa28498aa0a591
Forrest Beck <forrest.beck@da.org>
2019-06-20 10:29:37 -0400

    v.02

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 from distutils.core import setup
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name = 'veracross_api',
-    packages = ['veracross_api'],
-    version = '0.1',
-    license='MIT',
-    description = 'Simple library for interacting with the Veracross API',
-    author = 'Forrest Beck',
-    author_email = 'forrest.beck@da.org',
-    url = 'https://github.com/beckf/veracross_api',
-    download_url = 'https://github.com/beckf/veracross_api/archive/v.01.tar.gz',
-    keywords = ['Veracross', 'API'],
-    install_requires=[
-        'requests'
-    ],
+    name='veracross_api',
+    packages=['veracross_api'],
+    version='0.2',
+    description='Simple library for interacting with the Veracross API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    author='Forrest Beck',
+    author_email='forrest.beck@da.org',
+    url='https://github.com/beckf/veracross_api',
+    download_url='https://github.com/beckf/veracross_api/archive/v.02.tar.gz',
+    keywords=['Veracross', 'API'],
+    install_requires='requests',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
